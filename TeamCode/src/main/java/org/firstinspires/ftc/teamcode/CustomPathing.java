d124c436fc5bdb528dc6cfee48c864830eec7281
aarna ✨ <aarnaagrawl@gmail.com>
2026-07-14 12:27:36 -0700

    adding rotation before moving for custom pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomPathing.java
@@ -37,9 +37,16 @@ public class CustomPathing extends OpMode {
     double kPH = 0.002;
     double errorH = 0;
     double lastErrorH = 0;
-    double toleranceYH= 0.5;
+    double toleranceH= 0.5;
     double kDH = 0.0001;
     double rotate;
+    private enum State {
+        TURN,
+        MOVE,
+        DONE
+    }
+
+    private State state;
 
     @Override
     public void init() {
@@ -49,6 +56,11 @@ public class CustomPathing extends OpMode {
         pinpoint.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
         pinpoint.setPosition(startPose);
         drive.init(hardwareMap);
+        if (startPose.getHeading(AngleUnit.DEGREES) != endPose.getHeading(AngleUnit.DEGREES)) {
+            state = State.TURN;
+        } else {
+            state = State.MOVE;
+        }
     }
 
     public void start() {
@@ -58,43 +70,80 @@ public class CustomPathing extends OpMode {
 
     @Override
     public void loop() {
-        pinpoint.update();
-        currentPose = pinpoint.getPosition();
-        curTime = getRuntime();
-        errorX = endPose.getX(DistanceUnit.INCH) - currentPose.getX(DistanceUnit.INCH);
-        errorY = endPose.getY(DistanceUnit.INCH) - currentPose.getY(DistanceUnit.INCH);
-        double theta = currentPose.getHeading(AngleUnit.DEGREES);
-        errorH = endPose.getHeading(AngleUnit.DEGREES) - theta;
-        if (Math.abs(errorY) > toleranceY) {
-            double pTerm = errorY * kPY;
-            double dT = curTime - lastTime;
-            double dTerm;
-            if (dT != 0) {
-                dTerm = ((errorY - lastErrorY) / dT) * kDY;
+        if (state == State.TURN) {
+            pinpoint.update();
+            currentPose = pinpoint.getPosition();
+            curTime = getRuntime();
+            errorH = AngleUnit.normalizeDegrees(endPose.getHeading(AngleUnit.DEGREES)-currentPose.getHeading(AngleUnit.DEGREES));
+            if (Math.abs(errorH) > toleranceH) {
+                double pTerm = errorH * kPH;
+                double dT = curTime - lastTime;
+                double dTerm;
+                if (dT != 0) {
+                    dTerm = ((errorH - lastErrorH) / dT) * kDH;
+                } else {
+                    dTerm = 0;
+                }
+                rotate = Range.clip(pTerm + dTerm, -0.4, 0.4);
+                drive.setPower(0, 0, rotate, 1.0);
             } else {
-                dTerm = 0;
+                drive.setPower(0, 0, 0, 1.0);
+                state = State.MOVE;
+                lastErrorX = 0;
+                lastErrorY = 0;
             }
-            forward = Range.clip(pTerm + dTerm, -0.4, 0.4);
-        } else {
-            forward = 0;
-        }
+            lastTime = curTime;
+            lastErrorH = errorH;
+        } else if (state == State.MOVE) {
+            pinpoint.update();
+            currentPose = pinpoint.getPosition();
+            curTime = getRuntime();
+            double fieldErrorX = endPose.getX(DistanceUnit.INCH) - currentPose.getX(DistanceUnit.INCH);
+            double fieldErrorY = endPose.getY(DistanceUnit.INCH) - currentPose.getY(DistanceUnit.INCH);
+            double headingRad = Math.toRadians(currentPose.getHeading(AngleUnit.DEGREES));
+            double cosH = Math.cos(headingRad);
+            double sinH = Math.sin(headingRad);
+            errorX =  fieldErrorX * cosH + fieldErrorY * sinH;
+            errorY = -fieldErrorX * sinH + fieldErrorY * cosH;
 
-        if (Math.abs(errorX) > toleranceX) {
-            double pTerm = errorX * kPX;
-            double dT = curTime - lastTime;
-            double dTerm;
-            if (dT != 0) {
-                dTerm = ((errorX - lastErrorX) / dT) * kDX;
+            if (Math.abs(errorY) > toleranceY) {
+                double pTerm = errorY * kPY;
+                double dT = curTime - lastTime;
+                double dTerm;
+                if (dT != 0) {
+                    dTerm = ((errorY - lastErrorY) / dT) * kDY;
+                } else {
+                    dTerm = 0;
+                }
+                forward = Range.clip(pTerm + dTerm, -0.4, 0.4);
             } else {
-                dTerm = 0;
+                forward = 0;
             }
-            strafe = Range.clip(pTerm + dTerm, -0.4, 0.4);
-        } else {
-            strafe = 0;
+
+            if (Math.abs(errorX) > toleranceX) {
+                double pTerm = errorX * kPX;
+                double dT = curTime - lastTime;
+                double dTerm;
+                if (dT != 0) {
+                    dTerm = ((errorX - lastErrorX) / dT) * kDX;
+                } else {
+                    dTerm = 0;
+                }
+                strafe = Range.clip(pTerm + dTerm, -0.4, 0.4);
+            } else {
+                strafe = 0;
+            }
+
+            if ((forward == 0) && (strafe == 0)) {
+                state = State.DONE;
+            }
+
+            drive.setPower(strafe, forward, 0, 1.0);
+            lastErrorY = errorY;
+            lastErrorX = errorX;
+            lastTime = curTime;
+        } else if (state == State.DONE) {
+            drive.setPower(0, 0, 0, 1.0);
         }
-        drive.setPower(strafe, forward, 0, 1.0);
-        lastErrorY = errorY;
-        lastErrorX = errorX;
-        lastTime = curTime;
     }
 }
